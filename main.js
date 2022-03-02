69ee9f38c9002d9fb9d84e44ea805c2aa5bd1de9
yeonsiyeon <asdf748596123@gmail.com>
2022-03-02 23:29:47 +0900

    update/html

--- main.js
+++ main.js
@@ -15,7 +15,10 @@ const button1 = document.querySelector('.btn1');
 const button2 = document.querySelector('.btn2');
 const button3 = document.querySelector('.btn3');
 const button4 = document.querySelector('.btn4');
-
+const button1_toggle = document.querySelector('.btn1 a');
+const button2_toggle = document.querySelector('.btn2 a');
+const button3_toggle = document.querySelector('.btn3 a');
+const button4_toggle = document.querySelector('.btn4 a');
 
 function move(){
 
@@ -25,15 +28,9 @@ function move(){
 
         ul.style.transition = '0.2s';
         ul.style.transform = "translate3d(-"+1000*(curIndex+1)+"px, 0px, 0px)";
-    
-        curIndex++;
         
-        if(curIndex === 4){
-            setTimeout(function(){
-            ul.style.transition = '0s';
-            ul.style.transform = "translate3d(0px, 0px, 0px)"; },201)
-            curIndex = 0;
-        }
+        curIndex++;
+
     },3500); 
 /*Main image Slide Button*/
     function btn1(){
@@ -60,6 +57,124 @@ function move(){
     }
     button4.addEventListener('click', btn4);
 
+    setInterval(function(){
+        if(curIndex==0||curIndex==4){  
+            button1_toggle.classList.add('on');
+            button2_toggle.classList.remove('on');
+            button3_toggle.classList.remove('on');
+            button4_toggle.classList.remove('on');
+            }
+        else if(curIndex==1){   
+            button2_toggle.classList.add('on');
+            button1_toggle.classList.remove('on');
+            button3_toggle.classList.remove('on');
+            button4_toggle.classList.remove('on');
+        }
+        else if(curIndex==2){   
+            button3_toggle.classList.add('on');
+            button1_toggle.classList.remove('on');
+            button2_toggle.classList.remove('on');
+            button4_toggle.classList.remove('on');
+        }
+        else if(curIndex==3){   
+            button4_toggle.classList.add('on');
+            button1_toggle.classList.remove('on');
+            button2_toggle.classList.remove('on');
+            button3_toggle.classList.remove('on');
+        }
+        
+        if(curIndex === 4){
+            setTimeout(function(){
+            ul.style.transition = '0s';
+            ul.style.transform = "translate3d(0px, 0px, 0px)"; },201)
+            curIndex = 0;
+        }
+    })
+
 }
 document.addEventListener("DOMContentLoaded",function(){move();});
 
+/*About2 Typing*/
+let target = document.querySelector("#dynamic");
+
+function randomString(){
+    let stringArr = ["#Love is love", "#Love yourself","#Love is unity"];
+    let selectString = stringArr[Math.floor(Math.random()*stringArr.length)];
+    let selectStringArr = selectString.split("");
+
+    return selectStringArr;
+}
+
+function resetTyping(){
+    target.textContent = "";
+    dynamic(randomString());
+}
+
+function dynamic(randomArr){
+    if(randomArr.length>0){
+        target.textContent += randomArr.shift();
+        setTimeout(function(){
+            dynamic(randomArr);
+        }, 80); 
+    }else{
+        setTimeout(resetTyping, 3000);
+    }
+}
+
+dynamic(randomString());
+
+function blink(){
+    target.classList.toggle("active");
+}
+setInterval(blink,500);
+
+/*Roadmap Scroll*/
+var animation = function () {
+    var items, winH;
+
+    var initModule = function () {
+      items = document.querySelectorAll(".content2");
+      winH = window.innerHeight;
+      _addEventHandlers();
+    }
+
+    var _addEventHandlers = function () {
+      window.addEventListener("scroll", _checkPosition);
+      window.addEventListener("load", _checkPosition);
+      window.addEventListener("resize", initModule);
+    };
+
+    var _checkPosition = function () {
+      for (var g = 0; g < items.length; g++) {
+        var posFromTop = items[g].getBoundingClientRect().top;
+        if(winH<= posFromTop){
+            items[g].classList.remove("active")
+        }else if (winH - 100> posFromTop) {
+            items[g].classList.add("active")
+        }
+      }
+    }
+
+    return {
+      init: initModule
+    }
+  }
+
+  animation().init();
+
+/*FAQ Button*/
+const items = document.querySelectorAll(".accordion button");
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute('aria-expanded');
+  
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute('aria-expanded', 'false');
+  }
+  
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
